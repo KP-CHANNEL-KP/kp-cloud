644aad46c6a98e78ab6dbc8b9b6f3cebc6666fc5
KP-CHANNEL-KP <kopaing232003@gmail.com>
2026-03-21 01:50:40 +0630

    Fix: Modularize auth options for edge compatibility

---
+++ src/lib/auth.tssrc/lib/auth.ts
@@ -0,0 +1,16 @@
+import GitHubProvider from "next-auth/providers/github";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  // Cloudflare အတွက် session ကို jwt သုံးဖို့ အသေအချာ သတ်မှတ်ပေးရပါတယ်
+  session: {
+    strategy: "jwt",
+  },
+};
