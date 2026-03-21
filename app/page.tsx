7336d529bdea528080d75d99f048f930047151b6
KP-CHANNEL-KP <kopaing232003@gmail.com>
2026-03-21 17:14:15 +0630

    remove prisma بالكامل

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>KP Cloud 🚀</h1>
+      <p>Welcome to my site</p>
+
+      <a href="/login">Login</a><br />
+      <a href="/register">Register</a>
+    </div>
+  );
+}
