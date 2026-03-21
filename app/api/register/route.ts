3e9e804fc56c716834afe6c800ad03ffba34fd08
KP-CHANNEL-KP <kopaing232003@gmail.com>
2026-03-21 16:59:40 +0630

    fix crypto

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,6 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
+export const runtime = "edge";
+
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
