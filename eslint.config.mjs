fec149687bfd391bca70b2816292887c817fb44b
KP-CHANNEL-KP <kopaing232003@gmail.com>
2026-03-21 16:34:55 +0630

    fix crypto

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 
-import nextVitals from "eslint-config-next/core-web-vitals.js";
+import nextVitals from "eslint-config-next/core-web-vitals";
 import typescript from "eslint-config-next/typescript.js";
+import nextTs from "eslint-config-next/typescript";
+
+
 
 export default defineConfig([
   nextVitals,
   typescript,
-
+  nextTs,
   globalIgnores([
     ".next/**",
     "out/**",
